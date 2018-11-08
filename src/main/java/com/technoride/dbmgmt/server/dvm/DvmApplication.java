392b6c98e3e31b80035501a9fb393db96ce11c3b
vipin810 <vipinnagar810@gmail.com>
2018-11-08 16:39:17 +0530

    initial commit

---
+++ src/main/java/com/technoride/dbmgmt/server/dvm/DvmApplication.java
@@ -0,0 +1,12 @@
+package com.technoride.dbmgmt.server.dvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/technoride/dbmgmt/server/dvm/DvmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.technoride.dbmgmt.server.dvm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DvmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
